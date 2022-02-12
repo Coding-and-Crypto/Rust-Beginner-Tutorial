20a72a9f53e9eede5bb0d0ab00d2c9749f47d664
jpcaulfi <joecaulfield29@yahoo.com>
2022-02-12 15:30:25 -0500

    basic operations

---
+++ the-basics/src/basic-operations.rs
@@ -0,0 +1,45 @@
+// Function returns
+fn give_me_slice() -> &'static str {
+    return "pizza"
+}
+fn give_me_string() -> String {
+    String::from("pizza")
+}
+fn give_me_int() -> u32 {
+    4
+}
+
+// Conditionals
+fn conditional() {
+    let number = 3;
+
+    if number < 5 {
+        println!("True")
+    } else if number == 5 {
+        println!("OK")
+    } else {
+        println!("False")
+    }
+}
+
+// Loops
+fn while_loop() {
+    loop {
+        println!("Infinite loop!!!");
+    };
+}
+fn for_loop() {
+    for n in 1..40 {
+        println!("Hello #{}", n);
+    }
+}
+fn loop_as_variable() {
+    let mut counter = 0;
+    let g = loop {
+        if counter == 3 {
+            break {
+                "donut"
+            };
+        }
+    };
+}
