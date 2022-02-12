c04f576f1bbc2c52d780bcb3ecc3da9c1b5fce4c
jpcaulfi <joecaulfield29@yahoo.com>
2022-02-12 15:29:20 -0500

    primitives

--- the-basics/src/main.rs
+++ the-basics/src/main.rs
@@ -1,53 +1,5 @@
 
-// Can't overwrite an immutable var
-fn immutable() {
-    let x = 5;
-    // x = 6;
-}
 
-// Must overwrite with "let"
-fn overwrite() {
-    let x = 5;
-    let x = 6;
-}
-
-// Can't mutate a variable's type
-fn mutate_type() {
-    let some_string = "donut";
-    // some_string = some_string.len();
-}
-
-// Must mutate with "let"
-fn overwrite_type() {
-    let some_string = "donut";
-    let some_string = some_string.len();
-}
-
-// Declaring data types
-fn declare() {
-    let x: u32 = 20;
-}
-
-// Integers
-fn integer() {
-    let signed_int: i64 = 34;
-    let unsigned_int: u16 = 11; 
-}
-
-// Strings
-fn strings() {
-    let some_string = "donut"; // Slice (str)
-    let some_other_string: String = String::from("donut"); // String Object (String)
-}
-
-// Slices
-fn slices() {
-    // let some_string: str = "donut";
-    let some_other_string: &'static str = "donut";
-}
-
-// Constants
-const SOME_CONSTANT: u32 = 20;
 
 // Function returns
 fn give_me_slice() -> &'static str {
