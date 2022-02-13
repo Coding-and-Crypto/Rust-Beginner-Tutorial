90f668bd3d84a58241f4d346f311960a9e6b421c
jpcaulfi <joecaulfield29@yahoo.com>
2022-02-13 17:29:28 -0500

    setup example

--- the-basics/src/borrowing.rs
+++ the-basics/src/borrowing.rs
@@ -1,3 +1,19 @@
 
 
-// TODO: Borrowing
+// References
+fn references() {
+
+    let mut original_value: i32 = 20;
+
+    // Borrow the original value to do some stuff
+    let x = &original_value; // & is a reference
+
+    // We can't do this operation because a reference to "a" is required later in the program
+    // Rust's **borrower compiler** guarantees your reference won't change
+    // If we conducted this operation, we would re-allocate this variable's location on the Heap
+    //   and x would point to something else or nothing
+    original_value = original_value*3;
+
+    println!("{}", x)
+}
+
