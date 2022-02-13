4911af1884424375283f9f0c89fbfa724e159ad8
jpcaulfi <joecaulfield29@yahoo.com>
2022-02-13 12:48:56 -0500

    setup examples

--- the-basics/src/options.rs
+++ the-basics/src/options.rs
@@ -1,28 +1,35 @@
+
 // Rust does not have null
 // Instead we use Options
-enum Option<T> {
-    Some(T),
-    None,
-}
+// enum Option<T> {
+//     Some(T),
+//     None,
+// }
 
-fn testing_options() {
-    let some_number = Some(5);
-    let some_string = Some("a string");
-    let absent_number: Option<i32> = Option::None;
-}
-
-// Matching
-enum Coin {
-    Penny,
-    Nickel,
-    Dime,
-    Quarter,
-}
-
-fn value_in_cents(coin: Coin) -> u8 {
-    match coin {
-        Coin::Penny => 1,
-        Coin::Nickel => 5,
-        Coin::Dime => 10,
-        CoinL::Quarter => 25,
+fn option_example() {
+    let mut vector = vec![1, 2, 3];
+    for i in (0..5) {
+        let x = match vector.pop() {
+            Some(val) => val,
+            None => 0,
+        };
+        println!("{}", x);
     }
+}
+
+// Match
+fn match_this(val: i32) -> String {
+    match val {
+        1 => "One".to_string(),
+        2 => "Two".to_string(),
+        3 => "Three".to_string(),
+        _ => "Not listed".to_string(),
+    }
+}
+
+fn match_example() {
+    println!("{}", match_this(1));
+    println!("{}", match_this(2));
+    println!("{}", match_this(3));
+    println!("{}", match_this(4));
+}
