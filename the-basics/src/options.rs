1b420f171a4fcec64696d1423d7d798639342d70
jpcaulfi <joecaulfield29@yahoo.com>
2022-02-13 11:31:49 -0500

    setup examples

--- the-basics/src/options.rs
+++ the-basics/src/options.rs
@@ -11,3 +11,18 @@ fn testing_options() {
     let absent_number: Option<i32> = Option::None;
 }
 
+// Matching
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter,
+}
+
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        CoinL::Quarter => 25,
+    }
