0573168bdd90c2a2963456275ee643b20b1627a7
jpcaulfi <joecaulfield29@yahoo.com>
2022-02-12 19:09:41 -0500

    borrowing file

--- the-basics/src/main.rs
+++ the-basics/src/main.rs
@@ -1,36 +1,5 @@
 
 
-// Enum
-enum Direction {
-    Forward,
-    Backward,
-}
-
-fn enum_example() {
-    let player_direction: Direction = Direction::Forward;
-}
-
-
-// Enums also have methods
-
-enum Message {
-    Quit,
-    Move {x: u64, y: u64},
-    Write(String),
-    ChangeColor(i32, i32, i32),
-}
-
-impl Message {
-    fn call(&self) -> String {
-        String::from("Response")
-    }
-}
-
-fn example() {
-    let msg = Message::Move {x: 20, y: 40};
-    println!("{}", msg.call())
-}
-
 fn main() {
-    example()
+    print!("Hello, world!")
 }
